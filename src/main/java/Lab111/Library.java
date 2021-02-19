2cc88cb8dc6d2d0424fc785dd7db6b19fd434dd3
pangjai <pangjai@192.168.0.185>
2021-02-19 12:10:43 +0800

    I have made a change.

--- src/main/java/Lab111/Library.java
+++ src/main/java/Lab111/Library.java
@@ -5,8 +5,11 @@ package Lab111;
 
 public class Library {
 	
-	public static void main(String arg[]) {
-		System.out.println("Hello, World");
+	public static void main(String arg[]) {		
+		Library lib = new Library();
+		if (lib.someLibraryMethod())
+			System.out.println("Hello, World");
+			
 	}
     public boolean someLibraryMethod() {
         return true;
